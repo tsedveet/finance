d95ffb4ca3ecb96e56a8cf134dfa4edcb3d221b0
tsedveet <tsedvee.tt@gmail.com>
2021-06-24 22:43:45 +0800

    Orlogo zarlagiin delgetsen deer gargana

--- app.js
+++ app.js
@@ -16,6 +16,25 @@ var uiController = (function() {
         },
         getDOMstrings: function(){
             return DOMstrings;
+        },
+
+        addListItem: function(item, type){
+            // Orlogo zarlagiin elementiig aguulsan html-g beltgene.
+            var html, list;
+
+            if(type === 'inc'){
+                list = '.income__list';
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">+ $$VALUE$$</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            } else {
+                list = '.expenses__list';
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value">- $$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+            // Ter HTML dotroo orlogo zarlagiin utguudiig REPLACE ashiglaj uurchilj ugnu 
+            html = html.replace('%id%', item.id);
+            html = html.replace('$$DESCRIPTION$$', item.description);
+            html = html.replace('$$VALUE$$', item.value);
+            // Beltgesen HTML ee DOM ruu hiij ugnu.
+            document.querySelector(list).insertAdjacentHTML('beforeend', html); 
         }
     }
 })();
@@ -60,8 +79,9 @@ var financeController = (function() {
             } else {
                 item = new Expense(id, desc, val);
             }
-
             data.items[type].push(item);
+            return item;
+
         },
 
         seeData: function(){
@@ -78,8 +98,9 @@ var appController = (function(uiController, financeController) {
 
     console.log(input);
     // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
-    financeController.addItem(input.type, input.description, input.value);
+    var item = financeController.addItem(input.type, input.description, input.value);
     // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана
+    uiController.addListItem(item, input.type);
     // 4. Төсвийг тооцоолно
     // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
     };
