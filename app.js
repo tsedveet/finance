b24ef436f5123ac8962f80831d26c89664570079
tsedveet <tsedvee.tt@gmail.com>
2021-06-24 20:54:56 +0800

    finance module hiij ehelsen, Expense, Income objectuudiig uusgene

--- app.js
+++ app.js
@@ -1,30 +1,83 @@
-var uiController = (function(){
-
-})();
-
-var financeController = (function(){
-
-})();
-
-var appController = (function(uiController, fnController){
-    var ctrlAddItem = function(){
-        // 1. oruulah ugugdliig delgetsees olj avna
-        console.log('Delgetsnees ugugdul avah heseg');
-        // 2. olj avsan ugugdluudee sanhuugiin controllert damjuulj hadgalana
-
-        // 3. olj avsan ugugdluudee web deeree tohiroh hesegt n hadgalana
-
-        // 4. tusviig tootsoolno
-
-        // 5. etssiin uldegdel, tootsoog delgetsend gargana.
-    };
-    document.querySelector('.add__btn').addEventListener('click', function(){
-        ctrlAddItem();
-    });
-
-    document.addEventListener('keypress', function(event){
-        if(event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
+// Дэлгэцтэй ажиллах контроллер
+var uiController = (function() {
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        addBtn: '.add__btn'
+    }
+    return {
+        getInput: function(){
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value                
+            }
+        },
+        getDOMstrings: function(){
+            return DOMstrings;
         }
-    });
+    }
+})();
+
+// Санхүүтэй ажиллах контроллер
+var financeController = (function() {
+    var Income = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Expense = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems:{
+            inc: [],
+            exp: []
+        },
+
+        totals:{
+            inc: 0,
+            exp: 0
+        }
+    }
+    
+})();
+
+// Програмын холбогч контроллер
+var appController = (function(uiController, financeController) {
+    var ctrlAddItem = function() {
+    // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
+    console.log(uiController.getInput());
+    // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+    // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана
+    // 4. Төсвийг тооцоолно
+    // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
+    };
+
+    var setupEventListeners = function(){
+        var DOM = uiController.getDOMstrings();
+        document.querySelector(DOM.addBtn).addEventListener("click", function() {
+            ctrlAddItem();
+        });
+        
+        document.addEventListener("keypress", function(event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    }
+
+    return{
+        init: function(){
+            console.log('Application started...');
+            setupEventListeners();
+        }
+    }
 })(uiController, financeController);
+
+appController.init();
